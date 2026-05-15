6676c80199a3f1c6f21a39e45dc934583e14426f
entii <tienhuynh8337@gmail.com>
2026-05-15 19:09:28 +0700

    data_type

---
+++ src/data_type/object_data_type.js
@@ -0,0 +1,15 @@
+//object: key-value
+
+const person = {
+  name: "tien",
+  age: 20,
+  address: "can tho",
+};
+
+console.log(person);
+
+//array
+
+const scores = [10, 9, 2, 4, 7];
+
+console.log(scores);
---
+++ src/data_type/primitive_data_type.js
@@ -0,0 +1,31 @@
+/*=====================
+  Primitive Data Types
+ =====================*/
+
+//number
+
+const myAge = 20;
+const myScore = 9.5;
+
+console.log(myAge);
+console.log(myScore);
+
+//string
+
+const name = "ntien";
+const name2 = `tien ne`;
+
+console.log(name);
+
+//boolean: true/false
+const isCrushThichTui = true;
+
+console.log(isCrushThichTui);
+
+//undefine
+let bla;
+console.log(bla);
+
+//null
+let testNull = null;
+console.log(testNull);
