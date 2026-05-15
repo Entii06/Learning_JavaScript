954cb1cbd211cac1e8b1d02dbc4593adad1cee66
entii <tienhuynh8337@gmail.com>
2026-05-15 19:24:25 +0700

    operators

---
+++ src/operators/operators.js
@@ -0,0 +1,21 @@
+// let a = 5;
+// const b = 10;
+// const c = 15;
+
+// console.log(a + b);
+// console.log(c - a);
+// console.log(c % b);
+
+// const d = ++a;
+//++a tăng d = 6
+
+// const e = a++;
+//a++ gán cho e trước rồi mới cộng => e = 5
+
+const a = 5;
+const b = "5";
+
+console.log("check ==", a == b);
+console.log("check ===", a === b);
+
+// || && !
