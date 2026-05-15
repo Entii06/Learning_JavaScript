76d81d7ba7c88acc610029a57968af1925b76a84
entii <tienhuynh8337@gmail.com>
2026-05-15 19:49:57 +0700

    console_log

---
+++ src/console_log/console_log.js
@@ -0,0 +1,12 @@
+console.log("info"); //white
+console.error("oops my mistake"); // red
+console.warn("test warning"); // yellow
+
+let name = "enti";
+let age = 20;
+
+console.log(name, age);
+//don't need to use +
+console.log("Name: ", name, ", Age: ", age);
+
+console.log("%cCảnh báo!", "color: red; font-weight: bold;");
