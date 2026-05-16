c1cb944a594befd0a6d020c814a7d0089a2deda7
entii <tienhuynh8337@gmail.com>
2026-05-16 13:33:32 +0700

    functions

---
+++ src/functions/functions.js
@@ -0,0 +1,16 @@
+function greeting() {
+  console.log("say hi");
+}
+
+// const greeting = () => {
+//   console.log("Hello");
+// };
+
+greeting();
+
+function sum(a, b) {
+  return a + b;
+}
+
+console.log(sum(4, 5));
+console.log(sum("lap trinh web", " with ntien"));
