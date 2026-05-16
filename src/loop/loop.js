9223e4dbe621ef489ad63bbe36c68c06f351d2be
entii <tienhuynh8337@gmail.com>
2026-05-16 10:01:20 +0700

    loop

---
+++ src/loop/loop.js
@@ -0,0 +1,22 @@
+//for
+for (let i = 0; i <= 5; i++) {
+  console.log("i = ", i);
+}
+
+//while
+let score = 10;
+while (score > 7) {
+  console.log("score: ", score);
+  score--;
+}
+
+// do-while
+//chạy ít nhất 1 lần
+let score_1 = 8;
+do {
+  console.log("score_1: ", score_1);
+  score_1--;
+} while (score_1 > 2);
+
+//break bay ra khỏi vòng lặp
+//continue bỏ qua tất cả câu lệnh dưới nó chạy tiếp vòng lặp
