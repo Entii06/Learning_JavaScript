5ed8b04b57e3d2633655c3b2fea1c91449b862ce
entii <tienhuynh8337@gmail.com>
2026-05-18 19:06:02 +0700

    events

---
+++ src/events/events.js
@@ -0,0 +1,13 @@
+// event là hành động người dùng thao tác với website
+/*============================================
+Một số event thường gặp:
+click   Khi người dùng nhấn chuột
+input   Khi người dùng gõ vào ô nhập liệu
+change  Khi giá trị Input hoặc select thay đổi
+keydown Khi nhấn phím bất kì trên bàn phím
+Submit  Khi gửi form
+==============================================*/
+
+const handleClickBtn = () => {
+  console.log("you click a button");
+};
